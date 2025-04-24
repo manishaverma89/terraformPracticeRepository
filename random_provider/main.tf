9812b18dcfb761d001f640543fe3237ba9ff0d3d
manishaverma89 <84954924+manishaverma89@users.noreply.github.com>
2025-04-24 17:31:40 +0530

    main.tf

---
+++ random_provider/main.tf
@@ -0,0 +1,19 @@
+resource "random_id" "rand_id" {
+    byte_length = 8 
+}
+
+
+resource "aws_s3_bucket" "mytf-demobucket" {
+  bucket = "mytf-demobucket-${random_id.rand_id.hex}"
+}
+
+resource "aws_s3_object" "bucket-data" {
+    bucket = aws_s3_bucket.mytf-demobucket.bucket
+    source = "./myfile1.txt"
+    key = "mydata.txt"
+
+}
+  
+output "value-of-random-id" {
+  value = random_id.rand_id.hex
+}
